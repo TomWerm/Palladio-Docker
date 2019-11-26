9623f4970c3ccd298174e042f7c5a3fb372b4664
Thomas Weber <thomas.weber@student.kit.edu>
2019-11-26 15:19:38 +0100

    Adapted RunDockerImage, added error from container

--- EclipseEMFPalladio/RunDockerImage.cmd
+++ EclipseEMFPalladio/RunDockerImage.cmd
@@ -1,2 +1,5 @@
-docker run --rm -d emf:latest /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments"
+docker run --rm --interactive emf:latest /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments"
+FOR /F "tokens=*" %%g IN ('docker ps -q') do (SET CONTAINER_ID=%%g)
+docker cp %CONTAINER_ID%:/usr/experimentData "F:\Projekte\docker\docker\EclipseEMFPalladio\Output"
+docker stop %CONTAINER_ID%
 pause
--- EclipseEMFPalladio/RunExperimentAutomation.sh
+++ EclipseEMFPalladio/RunExperimentAutomation.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 /usr/eclipse/eclipse \
      -application org.palladiosimulator.experimentautomation.application \
      -consoleLog "$1" \
