013d4a5544a3533fbbe7165635e59537ef158bb5
Thomas Weber <thomas.weber@student.kit.edu>
2019-11-26 22:43:08 +0100

    Added output file to gitignore and modified cmd script

--- EclipseEMFPalladio/RunDockerImage.cmd
+++ EclipseEMFPalladio/RunDockerImage.cmd
@@ -1,5 +1,7 @@
-docker run --rm --interactive emf:latest /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments"
+docker run --rm -it emf:latest /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments" tail -f /dev/null
 FOR /F "tokens=*" %%g IN ('docker ps -q') do (SET CONTAINER_ID=%%g)
+IF [%CONTAINER_ID%] == [] GOTO skip
 docker cp %CONTAINER_ID%:/usr/experimentData "F:\Projekte\docker\docker\EclipseEMFPalladio\Output"
 docker stop %CONTAINER_ID%
+:skip
 pause
