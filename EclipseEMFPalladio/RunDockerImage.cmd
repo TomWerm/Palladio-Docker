8b9de6692f64db16230f8cc6c0c0694af7d42683
Thomas Weber <thomas.weber@student.kit.edu>
2019-12-18 10:58:48 +0100

    Moved copying the directory to the host script, docker file is not modified for testing

--- EclipseEMFPalladio/RunDockerImage.cmd
+++ EclipseEMFPalladio/RunDockerImage.cmd
@@ -1,7 +1,12 @@
-docker run --rm -it emf:latest /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments" tail -f /dev/null
+SET SRC_PATH=F:\Projekte\docker\docker\EclipseEMFPalladio
+SET IMAGE_NAME=emf:latest
+docker run -it -d %IMAGE_NAME%
 FOR /F "tokens=*" %%g IN ('docker ps -q') do (SET CONTAINER_ID=%%g)
-IF [%CONTAINER_ID%] == [] GOTO skip
-docker cp %CONTAINER_ID%:/usr/experimentData "F:\Projekte\docker\docker\EclipseEMFPalladio\Output"
+FOR /F "tokens=*" %%g IN ('docker ps --format "{{.Names}}"') do (SET CONTAINER_NAME=%%g)
+docker cp "%SRC_PATH%/ExperimentData" %CONTAINER_NAME%:/usr/experimentData
+docker exec -it %CONTAINER_NAME% /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments"
+REM Use these commands for background execution
+REM docker exec -it -d %CONTAINER_NAME% /usr/RunExperimentAutomation.sh "/usr/experimentData/model/Experiments/Capacity.experiments"
+REM timeout /T 30
+docker cp %CONTAINER_NAME%:/usr/experimentData "%SRC_PATH%\Output"
 docker stop %CONTAINER_ID%
-:skip
-pause
