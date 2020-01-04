4457e2f8b58fa5c1785959a5797a703de535c491
Thomas Weber <thomas.weber@student.kit.edu>
2020-01-04 13:09:21 +0100

    Restructured projects, removed output and logs, added PalladioExperimentAutomation Dockerfile

--- PalladioRunExperiment/RunExperimentAutomation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-Xvfb :99 -screen 0 1920x1080x16 &
-export DISPLAY=:99
-/usr/eclipse/eclipse \
-     -clean \
-     -application org.palladiosimulator.experimentautomation.application \
-     -consoleLog "$1" \
-     -data "/usr/workspace"
--- PalladioRunExperiment/SetupGTK.sh
+++
@@ -1,3 +0,0 @@
-apt install xvfb libgtk-3-0
-Xvfb :99 -screen 0 1920x1080x16 &
-export DISPLAY=:99
