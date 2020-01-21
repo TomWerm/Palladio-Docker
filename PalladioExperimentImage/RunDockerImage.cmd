2e04cbc6dbb10ad685bca3df8a0475c8f56c2731
Thomas Weber <thomas.weber@student.kit.edu>
2020-01-21 16:37:05 +0100

    Changed eclipse version due to installation problems

--- PalladioExperimentImage/RunDockerImage.cmd
+++ PalladioExperimentImage/RunDockerImage.cmd
@@ -13,7 +13,6 @@ docker run -it -d %IMAGE_NAME%
 FOR /F "tokens=*" %%g IN ('docker ps -q') do (SET CONTAINER_ID=%%g)
 FOR /F "tokens=*" %%g IN ('docker ps --format "{{.Names}}"') do (SET CONTAINER_NAME=%%g)
 docker exec -it %CONTAINER_NAME% /usr/RunExperimentAutomation.sh %CONTAINER_PATH%%EXPERIMENTS_FILE_DIR%%EXPERIMENT_FILE_NAME% %CONTAINER_PATH%%EXPERIMENTS_FILE_DIR%%EXPERIMENT_GEN_FILE_NAME%
-REM in case you need to do some other tasks after the experiment, uncomment the next line
 docker cp %CONTAINER_NAME%:/result "%SRC_PATH%\Output"
 docker stop %CONTAINER_ID%
 PAUSE
